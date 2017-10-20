6057fe9be2f56c7358bd8d9cf6672b798803abe6
l1va <lev_matematik@tut.by>
2017-10-20 19:52:09 +0300

    lang swither improved

--- lang_switcher.php
+++ lang_switcher.php
@@ -1,17 +1,23 @@
+
+<div class="lang-switch">
+	<ul>
+		<li id="language-en">
+			<a href="?lang=en">
+				<span>EN</span>
+			</a>
+		</li>
+		<li id="language-ru">
+			<a href="?lang=ru">
+				<span>RU</span>
+			</a>
+		</li>
+	</ul>
+</div>
+/**
+OR:
 		<div class="top-lang-switch">
 			<?php 
 				if ( function_exists ( 'wpm_language_switcher' ) ) wpm_language_switcher ();
 			?>
-		</div>
-
-/**
-OR:
-		<div>
-			<a href="?lang=ru" class="has-image"     style="text-decoration:none">
-				<img src="/wp-content/plugins/wp-multilang/flags/ru.png" alt="Russian (RU)"/>
-			</a>
-			<a href="?lang=en" class="has-image"     style="text-decoration:none">
-				<img src="/wp-content/plugins/wp-multilang/flags/en.png" alt="English (US)"/>
-			</a>
 		</div>
 */
